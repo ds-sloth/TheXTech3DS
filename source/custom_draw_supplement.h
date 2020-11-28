6854f16bc97fda57a08c520307d83d5d90ce6f3e
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2020-11-27 20:05:05 -0500

    Reverted previous commit.

--- source/custom_draw_supplement.h
+++ source/custom_draw_supplement.h
@@ -1,46 +1,30 @@
-#include "citro2d.h"
+const C2D_ImageTint shadowTint =
+{C2D_Color32(0,0,0,255), 1.,
+C2D_Color32(0,0,0,255), 1.,
+C2D_Color32(0,0,0,255), 1.,
+C2D_Color32(0,0,0,255), 1.};
 
 inline bool C2D_DrawImage_Custom(C2D_Image img, float x, float y, float src_x, float src_y, float src_w, float src_h, bool shadow)
 {
-    C2Di_Context* ctx = C2Di_GetContext();
-    if (!(ctx->flags & C2DiF_Active))
-        return false;
-    if (6 > (ctx->vtxBufSize - ctx->vtxBufPos))
-        return false;
+    const Tex3DS_SubTexture* old_subtex = img.subtex;
+    // assuming not rotated (it isn't here)
+    float scale_x = (old_subtex->right - old_subtex->left)/old_subtex->width;
+    float scale_y = (old_subtex->bottom - old_subtex->top)/old_subtex->height;
+    const Tex3DS_SubTexture new_subtex = {
+        src_w,
+        src_h,
+        old_subtex->left + src_x*scale_x,
+        old_subtex->top + src_y*scale_y,
+        old_subtex->left + (src_x + src_w)*scale_x,
+        old_subtex->top + (src_y + src_h)*scale_y,
+    };
+    img.subtex = &new_subtex;
 
-    C2Di_SetTex(img.tex);
-    C2Di_Update();
-
-    // Calculate texcoords
-    float tex_scale_x = (img.subtex->right - img.subtex->left)/img.subtex->width;
-    float tex_scale_y = (img.subtex->bottom - img.subtex->top)/img.subtex->height;
-    float u1 = img.subtex->left + src_x*tex_scale_x;
-    float u2 = img.subtex->left + (src_x+src_w)*tex_scale_x;
-    float v1 = img.subtex->top + src_y*tex_scale_y;
-    float v2 = img.subtex->top + (src_y+src_h)*tex_scale_y;
-
-    static const C2D_Tint s_defaultTint = { (uint32_t) 0xFF<<24, 0.0f };
-    static const C2D_Tint s_shadowTint = {C2D_Color32(0,0,0,255), 1.};
-    if (!shadow)
-    {
-        C2Di_AppendVtx(x,       y,       0.f, u1, v1, 0, s_defaultTint.blend, s_defaultTint.color);
-        C2Di_AppendVtx(x,       y+src_h, 0.f, u1, v2, 0, s_defaultTint.blend, s_defaultTint.color);
-        C2Di_AppendVtx(x+src_w, y+src_h, 0.f, u2, v2, 0, s_defaultTint.blend, s_defaultTint.color);
-
-        C2Di_AppendVtx(x,       y,       0.f, u1, v1, 0, s_defaultTint.blend, s_defaultTint.color);
-        C2Di_AppendVtx(x+src_w, y+src_h, 0.f, u2, v2, 0, s_defaultTint.blend, s_defaultTint.color);
-        C2Di_AppendVtx(x+src_w, y,       0.f, u2, v1, 0, s_defaultTint.blend, s_defaultTint.color);
-    }
+    bool result;
+    if (shadow)
+        result = C2D_DrawImageAt(img, x, y, 0.f, &shadowTint);
     else
-    {
-        C2Di_AppendVtx(x,       y,       0.f, u1, v1, 0, s_shadowTint.blend, s_shadowTint.color);
-        C2Di_AppendVtx(x,       y+src_h, 0.f, u1, v2, 0, s_shadowTint.blend, s_shadowTint.color);
-        C2Di_AppendVtx(x+src_w, y+src_h, 0.f, u2, v2, 0, s_shadowTint.blend, s_shadowTint.color);
-
-        C2Di_AppendVtx(x,       y,       0.f, u1, v1, 0, s_shadowTint.blend, s_shadowTint.color);
-        C2Di_AppendVtx(x+src_w, y+src_h, 0.f, u2, v2, 0, s_shadowTint.blend, s_shadowTint.color);
-        C2Di_AppendVtx(x+src_w, y,       0.f, u2, v1, 0, s_shadowTint.blend, s_shadowTint.color);
-    }
-
-    return true;
+        result = C2D_DrawImageAt(img, x, y, 0.f);
+    img.subtex = old_subtex;
+    return result;
 }
